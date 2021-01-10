4c7dfbe5a8149059dc9370cfc4959f67efd1ea20
pedroyoung95 <jty9531@gmail.com>
2021-01-10 18:15:48 +0900

    1/10 18:15 인프런 수업내용

--- 클래스/상속.py
+++ 클래스/상속.py
@@ -1,12 +1,18 @@
-class AttackUnit:
-    def __init__(self, name, hp, damage):
+class Unit:
+    def __init__(self, name, hp):
         self.name = name
         self.hp = hp
+
+class AttackUnit(Unit):
+    def __init__(self, name, hp, damage):
+        # 부모 클래스명을 자식 클래스의 파라미터로 넣음
+        # 부모 클래스의 변수를 사용하려면 parentClass.__init__(self,...)으로 부모클래스의 __init__메소드를 호출
+        Unit.__init__(self, name, hp)
         self.damage = damage
 
     def attack(self, location):
         print("{} : {} 방향으로 적군을 공격합니다. [공격력 {}]".format(self.name, location, self.damage))
-        
+
     def damaged(self, damage):
         print("{} : {} 데미지를 입었습니다.".format(self.name, damage))
         self.hp -= damage
